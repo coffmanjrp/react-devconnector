6a5fd3ce4ddd5dfdf6fcb20b8071408fe8cfcb97
coffmanjrp <coffmanjrp@gmail.com>
2021-07-03 15:29:35 -0700

    Connecting to mondoDB with mongoose

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const connectDB = require('./config/db');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
+
+// Connect Database
+connectDB();
 
 app.get('/', (req, res) => res.send('API running'));
 
