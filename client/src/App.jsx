5e721db1b74f8bb1e14906bee2941db81678c2a4
coffmanjrp <coffmanjrp@gmail.com>
2021-07-06 09:59:32 -0700

    Clean up and initial components

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { Landing, Navbar } from './components/layout';
+import './App.css';
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Landing />
+    </>
+  );
+};
+
+export default App;
