0bb6bb4780312664e9633c8ce308040bcbe5a43b
coffmanjrp <coffmanjrp@gmail.com>
2021-07-06 15:33:34 -0700

    Create redux store

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,24 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import { Login, Register } from './components/auth';
 import { Landing, Navbar } from './components/layout';
+import store from './store';
 import './App.css';
 
 const App = () => {
   return (
-    <Router>
-      <Navbar />
-      <Route exact path="/" component={Landing} />
-      <section className="container">
-        <Switch>
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/register" component={Register} />
-        </Switch>
-      </section>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <section className="container">
+          <Switch>
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Register} />
+          </Switch>
+        </section>
+      </Router>
+    </Provider>
   );
 };
 
