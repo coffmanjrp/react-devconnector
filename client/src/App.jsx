a743bb13663014be23253197ed6f7a84788c8259
coffmanjrp <coffmanjrp@gmail.com>
2021-07-07 12:34:50 -0700

    Implement load user and set auth token

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,34 @@
+import { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { Login, Register } from './components/auth';
 import { Alert, Landing, Navbar } from './components/layout';
 import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
 import './App.css';
 
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
 const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
   return (
     <Provider store={store}>
       <Router>
         <Navbar />
         <Route exact path="/" component={Landing} />
-        <section className="container">
-          <Alert />
-          <Switch>
+        <Alert />
+        <Switch>
+          <section className="container">
             <Route exact path="/login" component={Login} />
             <Route exact path="/register" component={Register} />
-          </Switch>
-        </section>
+          </section>
+        </Switch>
       </Router>
     </Provider>
   );
