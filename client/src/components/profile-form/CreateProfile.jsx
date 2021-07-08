18f49b91d5d1fda8244cc4310171d0c30753adfd
coffmanjrp <coffmanjrp@gmail.com>
2021-07-07 22:56:28 -0700

    Create profile action

--- client/src/components/profile-form/CreateProfile.jsx
+++ client/src/components/profile-form/CreateProfile.jsx
@@ -1,8 +1,10 @@
 import { useState } from 'react';
+import { Link, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { createProfile } from '../../actions/profile';
 import PropTypes from 'prop-types';
 
-const CreateProfile = (props) => {
+const CreateProfile = ({ createProfile, history }) => {
   const [displaySocialInputs, toggleSocialInputs] = useState(false);
   const [formData, setFormData] = useState({
     company: '',
@@ -37,6 +39,11 @@ const CreateProfile = (props) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
+  const onSubmit = (e) => {
+    e.preventDefault();
+    createProfile(formData, history);
+  };
+
   return (
     <>
       <h1 className="large text-primary">Create Your Profile</h1>
@@ -45,7 +52,7 @@ const CreateProfile = (props) => {
         profile stand out
       </p>
       <small>* = required field</small>
-      <form className="form">
+      <form className="form" onSubmit={(e) => onSubmit(e)}>
         <div className="form-group">
           <select name="status" value={status} onChange={(e) => onChange(e)}>
             <option value="0">* Select Professional Status</option>
@@ -206,14 +213,16 @@ const CreateProfile = (props) => {
         )}
 
         <input type="submit" className="btn btn-primary my-1" />
-        <a className="btn btn-light my-1" href="dashboard.html">
+        <Link to="dashboard" className="btn btn-light my-1">
           Go Back
-        </a>
+        </Link>
       </form>
     </>
   );
 };
 
-CreateProfile.propTypes = {};
+CreateProfile.propTypes = {
+  createProfile: PropTypes.func.isRequired,
+};
 
-export default connect()(CreateProfile);
+export default connect(null, { createProfile })(withRouter(CreateProfile));
