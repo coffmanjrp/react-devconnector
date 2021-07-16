f04fffdfe3aa0a4d4fc57777d1094ed03c496ceb
coffmanjrp <coffmanjrp@gmail.com>
2021-07-16 13:05:59 -0700

    Logout from all tabs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,18 +4,25 @@ import { Provider } from 'react-redux';
 import { Landing, Navbar } from './components/layout';
 import { Routes } from './components/routing';
 import store from './store';
-import { loadUser } from './actions/auth';
+import { loadUser, logout } from './actions/auth';
 import setAuthToken from './utils/setAuthToken';
 import './App.css';
 
 const App = () => {
-  // check for token in LS
-  if (localStorage.token) {
-    setAuthToken(localStorage.token);
-  }
-
   useEffect(() => {
+    // check for token in LS
+    if (localStorage.token) {
+      setAuthToken(localStorage.token);
+    }
+
     store.dispatch(loadUser());
+
+    // log user out from all tabs if they log out in one tab
+    window.addEventListener('storage', () => {
+      if (!localStorage.token) {
+        logout();
+      }
+    });
   }, []);
 
   return (
